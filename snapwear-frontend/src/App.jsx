407c15cc0ad6fb63cd8bdec03741efb9cd164e98
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-04-03 11:24:49 +0530

    Update App.jsx

--- snapwear-frontend/src/App.jsx
+++ snapwear-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
