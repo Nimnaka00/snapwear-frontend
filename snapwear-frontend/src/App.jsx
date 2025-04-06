f594ee1e4a3e6b1580f1b5719fd51d912489c2b1
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-04-06 13:13:50 +0530

    Update App.jsx

--- snapwear-frontend/src/App.jsx
+++ snapwear-frontend/src/App.jsx
@@ -1,8 +1,20 @@
+// src/App.jsx
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center text-2xl font-bold">
-      SnapWear Frontend Ready 🚀
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col font-poppins bg-white">
+        <Navbar />
+        <main className="flex-grow">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
