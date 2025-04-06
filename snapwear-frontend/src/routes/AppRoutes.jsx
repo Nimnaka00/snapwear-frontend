c33e64300120e0ca11dd5c58b9f4ea205b28bd88
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-04-06 14:48:49 +0530

    Update AppRoutes.jsx

--- snapwear-frontend/src/routes/AppRoutes.jsx
+++ snapwear-frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,17 @@
 // src/routes/AppRoutes.jsx
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Register from "../pages/auth/Register";
 
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+       {/* Auth */}
+      <Route path="/register" element={<Register />} />
+
     </Routes>
   );
 };
