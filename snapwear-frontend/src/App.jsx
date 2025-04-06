18c4d0c543c4c74de2bf3ffd67b23ad65c00eb97
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-04-06 12:48:48 +0530

    Create App.jsx

---
+++ snapwear-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center text-2xl font-bold">
+      SnapWear Frontend Ready 🚀
+    </div>
+  );
+};
+
+export default App;
