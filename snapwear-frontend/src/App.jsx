cf3e675a0326da5e1e91c84c13ea25ee3c30902a
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-04-06 13:52:50 +0530

    Update App.jsx

--- snapwear-frontend/src/App.jsx
+++ snapwear-frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
