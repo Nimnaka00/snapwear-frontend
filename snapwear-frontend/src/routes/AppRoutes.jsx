23604e1ec9201d928625164c160234221d79c5f5
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-27 18:57:30 +0530

    Update AppRoutes.jsx

--- snapwear-frontend/src/routes/AppRoutes.jsx
+++ snapwear-frontend/src/routes/AppRoutes.jsx
@@ -1,17 +1,18 @@
-// src/routes/AppRoutes.jsx
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/auth/Register";
-
+import Chatbot from "../pages/ai/Chatbot"; // <-- import your Chatbot page
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
 
-       {/* Auth */}
+      {/* Auth */}
       <Route path="/register" element={<Register />} />
 
+      {/* AI Chatbot */}
+      <Route path="/chatbot" element={<Chatbot />} />
     </Routes>
   );
 };
