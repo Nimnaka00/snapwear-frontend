d940acc406b415a1e4519554576c29edfca207a7
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-29 22:22:32 +0530

    Create products.jsx

---
+++ snapwear-frontend/src/data/products.jsx
@@ -0,0 +1,17 @@
+import img1 from "../assets/shop/grace-karin.png";
+import img2 from "../assets/shop/black-blouse.png";
+import img3 from "../assets/shop/red-blouse.png";
+import img4 from "../assets/shop/mustard.png";
+import img5 from "../assets/shop/batwing.png";
+import img6 from "../assets/shop/casual.png";
+
+const products = [
+  { id: 1, name: "GRACE KARIN", price: "LKR 2399.00", image: img1, category: "Women" },
+  { id: 2, name: "Wrap Black Blouse", price: "LKR 1899.00", image: img2, category: "Women" },
+  { id: 3, name: "Red Long Sleeve", price: "LKR 1999.00", image: img3, category: "Women" },
+  { id: 4, name: "Mustard blouse", price: "LKR 2899.00", image: img4, category: "Women" },
+  { id: 5, name: "Batwing Sleeves", price: "LKR 3499.00", image: img5, category: "Women" },
+  { id: 6, name: "Casual Long Sleeve", price: "LKR 4399.00", image: img6, category: "Women" },
+];
+
+export default products;
