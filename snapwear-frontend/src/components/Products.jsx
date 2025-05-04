df33b1e837e3b1510d61cb738ca793e4618840fa
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-04 23:52:20 +0530

    Update Products.jsx

--- snapwear-frontend/src/components/Products.jsx
+++ snapwear-frontend/src/components/Products.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-import products from "../data/products";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Products = ({ selectedProduct, onSelectProduct }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.error("Error loading products:", err));
+  }, []);
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
       {products.map((product) => (
         <div
-          key={product.id}
+          key={product._id}
           onClick={() => onSelectProduct(product)}
           className={`cursor-pointer w-[180px] h-[220px] rounded-[8px] border border-[#D6FFF6] flex flex-col  justify-between p-2 bg-[#1b1d25] ${
-            selectedProduct?.id === product.id
+            selectedProduct?._id === product._id
               ? "border-[#D6FFF6]"
               : "border-transparent"
           } hover:border-[#D6FFF6] transition`}
         >
           <img
-            src={product.image}
+            src={product.imageUrl}
             alt={product.name}
             className="w-full h-[120px] object-contain mb-2"
           />
           <p className="text-[16px] font-bold">{product.name}</p>
-          <p className="text-[16px] font-medium">Price : {product.price}</p>
+          <p className="text-[16px] font-medium">Price: LKR {product.price}.00</p>
         </div>
       ))}
     </div>
