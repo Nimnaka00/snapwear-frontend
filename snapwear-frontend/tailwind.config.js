2c0dd78df6f0ab656bcdbb55233bb14d7f9c2ea9
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-04 14:01:24 +0530

    Update tailwind.config.js

--- snapwear-frontend/tailwind.config.js
+++ snapwear-frontend/tailwind.config.js
@@ -6,6 +6,18 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        russianViolet: '#231651',
+        darkVoid: '#151419',
+        gluconGray: '#1B1A1F',
+        mintGreen: '#D6FFF6',
+        dustyGray: '#878787',
+        snow: '#FBFBFB',
+        textMain: '#0E0920',
+        bgColor: '#13151B',
+        lineGray: 'rgba(102, 102, 102, 0.25)', 
+        cardBg: 'rgba(251, 251, 251, 0.2)',  
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
@@ -13,4 +25,3 @@ export default {
   },
   plugins: [],
 }
-
