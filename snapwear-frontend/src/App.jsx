16bc134f351b563bd2afdd30c02367a2f5a7b00a
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-08 03:15:35 +0530

    Update App.jsx

--- snapwear-frontend/src/App.jsx
+++ snapwear-frontend/src/App.jsx
@@ -2,29 +2,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Shop from "./components/Shop";
-import TryOn from "./pages/ai/TryOn"; 
+import TryOn from "./pages/ai/TryOn";
 import Chatbot from "./pages/ai/Chatbot";
 import ProductPage from "./pages/product/ProductPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import ResetPassword from "./pages/auth/ResetPassword";
 import SetNewPassword from "./pages/auth/SetNewPassword";
+import OTPVerification from "./pages/auth/OTPVerification";
 import Product from "./components/Product";
-
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/resetpassword" element={<ResetPassword/>}/>
-        <Route path="/reset-password/:token" element={<SetNewPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/verify-otp" element={<OTPVerification />} />
+        <Route path="/set-new-password" element={<SetNewPassword />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/tryon" element={<TryOn />} />
-        <Route path="/chatbot" element={<Chatbot/>}/>
-        <Route path="/product1" element={<Product/>} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/product1" element={<Product />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </Router>
