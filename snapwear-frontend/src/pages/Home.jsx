a7e8ad05bd09734559549e6db431072bc1c9ef77
NImnaka <10899636@students.plymouth.ac.uk>
2025-05-20 01:38:11 +0530

    Update Home.jsx

--- snapwear-frontend/src/pages/Home.jsx
+++ snapwear-frontend/src/pages/Home.jsx
@@ -4,14 +4,22 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import AIAndVirtualTryOn from '../components/AIAndVirtualTryOn';
 import FeaturedProducts from "../components/FeaturedProducts";
+import OurServices from "../components/OurServices";
+import AboutSnapwear from "../components/AboutSnapwear";
+import ContactUsMessage from "../components/ContactUsMessage";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="bg-bgColor text-white">
+    <div className="bg-#23252D text-white">
       <Navbar />
       <HeroSection />
       <AIAndVirtualTryOn/>
       <FeaturedProducts/>
+      <OurServices/>
+      <AboutSnapwear/>
+      <ContactUsMessage/>
+      <Footer/>
     </div>
   );
 };
