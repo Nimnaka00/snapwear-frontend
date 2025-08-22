75b098beff1d2ee34e4b7cc3c2bb9f5914e49b0f
NImnaka <152169083+Nimnaka00@users.noreply.github.com>
2025-08-22 22:28:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        russianViolet: '#231651',
-        darkVoid: '#151419',
-        gluconGray: '#1B1A1F',
-        mintGreen: '#D6FFF6',
-        dustyGray: '#878787',
-        snow: '#FBFBFB',
-        textMain: '#0E0920',
-        bgColor: '#13151B',
-        lineGray: 'rgba(102, 102, 102, 0.25)', 
-        cardBg: 'rgba(251, 251, 251, 0.2)',  
-        footerBg: '#23252D'
+        russianViolet: "#231651",
+        darkVoid: "#151419",
+        gluconGray: "#1B1A1F",
+        mintGreen: "#D6FFF6",
+        dustyGray: "#878787",
+        snow: "#FBFBFB",
+        textMain: "#0E0920",
+        bgColor: "#13151B",
+        lineGray: "rgba(102, 102, 102, 0.25)",
+        cardBg: "rgba(251, 251, 251, 0.2)",
+        footerBg: "#23252D",
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
